a87bd66d5ff82480d463fc2a1622a3e3117e96a9
Prafullkumar Gaikwad <prafullg90@gmail.com>
2018-10-21 21:22:15 +0530

    Final Submission

--- src/App.js
+++ src/App.js
@@ -18,19 +18,12 @@ class App extends Component {
   }
   render() {
 
-    const style = {
-      
-        width: "100%",
-        padding: "12px 20px",
-        margin: "8px 0",
-        
-    
-    }
+  
     return (
       <div className="App">
        <h1>My First Assignment in React js !</h1>
        <UserOutput  name={this.state.name} age={this.state.age}/>
-       <UserInput style={style} name={this.state.name} changeName={this.changeNameHandler}/>
+       <UserInput name={this.state.name} changeName={this.changeNameHandler}/>
       </div>
     );
   }
--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const userInput = (props) =>{
+    const style = {
+        border: '2px solid red',
+        borderRadius: "4px",
+        width : "50%",
+        padding: "12px 20px",
+        
+    };
+
     return(
-        <input type="text" onChange={props.changeName} value={props.name}/>
+        <input  style={style} type="text" onChange={props.changeName} value={props.name}/>
     );
 }
 
